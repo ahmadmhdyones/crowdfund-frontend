4ff597fc1382a726baa69df42a7fd1bec505c59a
Ibrahim Hafez <ibrahim.hafez99@hotmail.com>
2022-12-24 18:08:10 +0300

    fix routing bug

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: "./",
+  reactStrictMode: false,
+  // assetPrefix: "./",
 };
 
 module.exports = nextConfig;
