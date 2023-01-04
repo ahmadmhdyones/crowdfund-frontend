c12b5eeb822d86b528bff5a15d79e84602bf0193
Ibrahim Hafez <ibrahim.hafez99@hotmail.com>
2023-01-04 19:11:36 +0300

    Update middleware

--- client/middleware.js
+++ client/middleware.js
@@ -4,12 +4,21 @@ import { protectedRoutes, authRoutes } from "./src/constants";
 export default function middleware(req) {
   const token = req.cookies.get("token")?.value;
   // (req.nextUrl.pathname.startsWith("/"));
-  if (protectedRoutes.includes(req.nextUrl.pathname) && !token) {
-    req.cookies.delete("token");
-    const response = NextResponse.redirect(new URL("/login", req.url));
-    req.cookies.delete("token");
-    return response;
+  for (let i = 0; i < protectedRoutes.length; i++) {
+    if (req.nextUrl.pathname.startsWith(protectedRoutes[i]) && !token) {
+      console.log(true);
+      req.cookies.delete("token");
+      const response = NextResponse.redirect(new URL("/login", req.url));
+      req.cookies.delete("token");
+      return response;
+    }
   }
+  // if (protectedRoutes.includes(req.nextUrl.pathname) && !token) {
+  //   req.cookies.delete("token");
+  //   const response = NextResponse.redirect(new URL("/login", req.url));
+  //   req.cookies.delete("token");
+  //   return response;
+  // }
   if (authRoutes.includes(req.nextUrl.pathname) && token) {
     return NextResponse.redirect(new URL("/", req.url));
   }
