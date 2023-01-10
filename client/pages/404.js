e9fd9944276ffb41ef6def675072a7abe9172fb7
Ibrahim Hafez <ibrahim.hafez99@hotmail.com>
2023-01-10 13:15:08 +0300

    Create 404 page

--- client/pages/404.js
+++ client/pages/404.js
@@ -1,3 +1,19 @@
+import styles from "../styles/404.module.css";
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div className={styles.html}>
+      <div className={styles.error}>404</div>
+      <br />
+      <br />
+      <span className={styles.info}>File not found</span>
+      <img
+        src="http://images2.layoutsparks.com/1/160030/too-much-tv-static.gif"
+        className={styles.static}
+      />
+    </div>
+  );
 }
+
+Custom404.getLayout = function (page) {
+  return <>{page}</>;
+};
