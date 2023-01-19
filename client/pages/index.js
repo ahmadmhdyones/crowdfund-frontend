d4c8dcb309f517f53c2bb0a30af43cbc363ff95f
Ibrahim Hafez <ibrahim.hafez99@hotmail.com>
2023-01-19 18:49:31 +0300

    Fix days left bug

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,13 +3,17 @@ import { useRouter } from "next/router";
 import FundCard from "../src/components/FundCard";
 import { CampaignAPI } from "../src/apis/campaignAPI";
 const Home = (props) => {
+  console.log(props.response);
   let campaigns = props.response;
   const router = useRouter();
   return (
     <div>
-      <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">
-        All Campaigns {campaigns.length}
-      </h1>
+      {campaigns.length && (
+        <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">
+          All Campaigns {campaigns.length}
+        </h1>
+      )}
+
       <div className="flex flex-wrap mt-[20px] gap-[26px]">
         {campaigns.length === 0 && (
           <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
@@ -34,7 +38,9 @@ export default Home;
 export async function getServerSideProps() {
   try {
     const response = await CampaignAPI.getAllDeployed();
+    console.log(response)
     return {
+      
       props: { response },
     };
   } catch (err) {
